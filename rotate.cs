bca9bf5e9c81ae62f1b157547b4b14be4a11d033
PatrykHajzer <hajzer.patryk@gmail.com>
2017-08-02 18:05:12 +0300

    Add files via upload

---
+++ rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * 1.0F;
+        
+
+        transform.RotateAround(Vector3.up, -rotX);
+        
+    }
+}
---
+++ rotateY.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateY : MonoBehaviour
+{
+    public Quaternion from = Quaternion.Euler(0, 0, 0);
+    public Quaternion to = Quaternion.Euler(new Vector3(180, 0, 0));
+
+    void OnMouseDrag()
+    {
+        float rotY = Input.GetAxis("Mouse Y") * 0.1F;
+        transform.rotation = Quaternion.Slerp(from, to, Time.time * 0.1F);
+        
+    }
+}
